f3cebfc081f014c733c1a0f7b697d53a633d8816
Minonono <clock2elizabeth@gmail.com>
2022-10-02 11:49:20 +0900

    ABC060B回答追加

---
+++ cmd/abc060.go
@@ -0,0 +1,24 @@
+// 問題: https://atcoder.jp/contests/abc060/tasks
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	abc060B()
+}
+
+func abc060B() {
+	var a, b, c int
+	fmt.Scanf("%d %d %d", &a, &b, &c)
+
+	for i := 0; i <= b; i++ {
+		if a*i % b == c {
+			fmt.Println("YES")
+			return
+		}
+	}
+
+	fmt.Println("NO")
+}
