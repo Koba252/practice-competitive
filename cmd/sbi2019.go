b73fab54cdfbdfeca50fa12b963224dbe7174a06
Minonono <clock2elizabeth@gmail.com>
2022-10-14 23:57:43 +0900

    BootcampのEasy No.4

---
+++ cmd/sbi2019.go
@@ -0,0 +1,29 @@
+// 問題: https://atcoder.jp/contests/sumitrust2019/tasks
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	sbi2019B()
+}
+
+func sbi2019B() {
+	var n int
+	fmt.Scanf("%d", &n)
+
+	x := float64(n) / 1.08
+	if x > float64(int(x)) {
+		x += 1
+	}
+
+	out := int(x)
+
+	if out * 108 >= n * 100 && out * 108 < 100 * (n + 1) {
+		fmt.Println(out)
+		return
+	}
+
+	fmt.Println(":(")
+}
