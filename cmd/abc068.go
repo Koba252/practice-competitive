882399ac555b61193c0e024cbcae771214a5979e
Minonono <clock2elizabeth@gmail.com>
2022-10-25 23:49:26 +0900

    BootcampEasyNo.11

---
+++ cmd/abc068.go
@@ -0,0 +1,37 @@
+// 問題: https://atcoder.jp/contests/abc068/tasks
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	abc068B()
+}
+
+func abc068B() {
+	var n int
+	fmt.Scan(&n)
+
+	var result int
+	var tmp int
+	for i := 1; i <= n; i++ {
+		var cnt int
+		a := i
+		for j := 1; j < 7; j++ {
+			if a % 2 == 0 {
+				a /= 2
+				cnt++
+			} else {
+				break
+			}
+		}
+
+		if cnt >= tmp {
+			tmp = cnt
+			result = i
+		}
+	}
+
+	fmt.Println(result)
+}
