2c5dd36e122aa26d683506169a6e3731451092f0
Minonono <clock2elizabeth@gmail.com>
2022-10-30 20:46:10 +0900

    BootcampEasyNo.16

---
+++ cmd/abc142.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	abc142C()
+}
+
+func abc142C() {
+	var n int
+	fmt.Scan(&n)
+
+	r := bufio.NewReader(os.Stdin)
+	in, _ := r.ReadString('\n')
+	arr := strings.Fields(in)
+
+	l := make([]int, n)
+	for i := range l {
+		a, _ := strconv.Atoi(arr[i])
+		l[a - 1] = i + 1
+	}
+
+	for _, a := range l {
+		fmt.Print(a, " ")
+	}
+}
