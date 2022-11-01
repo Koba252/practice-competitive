ef5aa98ed04654c2c3e0ee83f623bf12e058f5fe
Minonono <clock2elizabeth@gmail.com>
2022-11-01 20:57:19 +0900

    BootcampEasyNo.18

---
+++ cmd/abc122.go
@@ -0,0 +1,37 @@
+// 問題: https://atcoder.jp/contests/abc122/tasks
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	abc122B()
+}
+
+func abc122B() {
+	r := bufio.NewReader(os.Stdin)
+	in, _ := r.ReadString('\n')
+	arr := strings.Split(in, "")
+
+	var now, max int
+	for _, a := range arr {
+		if a == "A" || a == "C" || a == "G" || a == "T" {
+			now++
+		} else {
+			if now > max {
+				max = now
+			}
+			now = 0
+		}
+	}
+
+	if now > max {
+		max = now
+	}
+
+	fmt.Println(max)
+}
