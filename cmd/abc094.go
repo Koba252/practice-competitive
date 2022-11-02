080920be8f67da917a926074ca03e0f2ece05136
Minonono <clock2elizabeth@gmail.com>
2022-11-02 23:36:18 +0900

    BootcampEasyNo.19

---
+++ cmd/abc094.go
@@ -0,0 +1,40 @@
+// 問題: https://atcoder.jp/contests/abc094/tasks
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"strconv"
+)
+
+func main() {
+	abc094B()
+}
+
+func abc094B() {
+	var n, m, x int
+	fmt.Scanf("%d %d %d", &n, &m, &x)
+
+	r := bufio.NewReader(os.Stdin)
+	in, _ := r.ReadString('\n')
+	arr := strings.Fields(in)
+
+	var s, l int
+	for _, as := range arr {
+		a, _ := strconv.Atoi(as)
+
+		if a < x {
+			s++
+		} else {
+			l++
+		}
+	}
+
+	if s < l {
+		fmt.Println(s)
+	} else {
+		fmt.Println(l)
+	}
+}
