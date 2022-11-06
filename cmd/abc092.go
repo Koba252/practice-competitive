92fb3279461cd690cf6148920c4c892e0e4dbe8b
Minonono <clock2elizabeth@gmail.com>
2022-11-06 22:15:28 +0900

    BootcampEasyNo.23

---
+++ cmd/abc092.go
@@ -0,0 +1,35 @@
+// 問題: https://atcoder.jp/contests/abc092/tasks
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	abc092B()
+}
+
+func abc092B() {
+	var n, d, x int
+	fmt.Scanf("%d", &n)
+	fmt.Scanf("%d %d", &d, &x)
+
+	ans := n + x
+	for i := 0; i < n; i++ {
+		var a int
+		fmt.Scanf("%d", &a)
+
+		cnt := 1
+		for {
+			tmp := cnt * a + 1
+			if tmp > d {
+				break
+			}
+
+			ans++
+			cnt++
+		}
+	}
+
+	fmt.Println(ans)
+}
