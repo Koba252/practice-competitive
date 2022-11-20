df25040f3373cf4ff7b494f8868e9824cc7976b4
Minonono <clock2elizabeth@gmail.com>
2022-11-20 22:52:59 +0900

    BootcampEasyNo.26

---
+++ cmd/abc153.go
@@ -0,0 +1,29 @@
+// 問題: https://atcoder.jp/contests/abc153/tasks
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	abc153D()
+}
+
+func abc153D() {
+	var h int
+	fmt.Scanf("%d", &h)
+
+	var ans int = 1
+	var monsterNum int = 1
+	for {
+		h /= 2
+		if h == 0 {
+			break
+		}
+
+		monsterNum *= 2
+		ans += monsterNum
+	}
+
+	fmt.Println(ans)
+}
