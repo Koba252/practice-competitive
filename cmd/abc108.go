aff7b9fb04e15c9adc0e3608e2d2a661c65d52cc
Minonono <clock2elizabeth@gmail.com>
2022-12-27 23:02:06 +0900

    BootcampEasyNo.30

---
+++ cmd/abc108.go
@@ -0,0 +1,25 @@
+// 問題: https://atcoder.jp/contests/abc108/tasks
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	abc108B()
+}
+
+func abc108B() {
+	var x1, y1, x2, y2, x3, y3, x4, y4 int
+	fmt.Scanf("%d %d %d %d", &x1, &y1, &x2, &y2)
+
+	xDiff := x1 - x2
+	yDiff := y1 - y2
+
+	x3 = x2 + yDiff
+	y3 = y2 - xDiff
+	x4 = x1 + yDiff
+	y4 = y1 - xDiff
+
+	fmt.Println(x3, y3, x4, y4)
+}
