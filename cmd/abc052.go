08d28dc07b50e5b9ad3ba2565770228fe666d4eb
Minonono <clock2elizabeth@gmail.com>
2023-01-01 23:27:35 +0900

    BootcampEasyNo.32

---
+++ cmd/abc052.go
@@ -0,0 +1,35 @@
+// 問題: https://atcoder.jp/contests/abc052/tasks
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	abc052B()
+}
+
+func abc052B() {
+	var n int
+	var s string
+	fmt.Scanf("%d", &n)
+	fmt.Scanf("%s", &s)
+
+	l := strings.Split(s, "")
+
+	var max, cnt int
+	for i := 0; i < n; i++ {
+		if l[i] == "I" {
+			cnt++
+		} else {
+			cnt--
+		}
+
+		if cnt > max {
+			max = cnt
+		}
+	}
+
+	fmt.Println(max)
+}
