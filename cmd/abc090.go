be643c3bd151bcced084675f419a0a6f173ae751
Minonono <clock2elizabeth@gmail.com>
2023-05-17 23:58:59 +0900

    BootcampEasyNo.45

---
+++ cmd/abc090.go
@@ -0,0 +1,38 @@
+// 問題: https://atcoder.jp/contests/abc090/tasks
+package main
+
+import (
+	"fmt"
+	"strings"
+	"strconv"
+)
+
+func main() {
+	abc090B()
+}
+
+func abc090B() {
+	var a, b int
+	fmt.Scanf("%d %d", &a, &b)
+
+	var ans int
+	for i := a; i <= b; i++ {
+		s := strconv.Itoa(i)
+		array := strings.Split(s, "")
+		len := len(array)
+		half := len / 2
+		hasDif := false
+		for j := 0; j < half; j++ {
+			if array[j] != array[len-1-j] {
+				hasDif = true
+				break
+			}
+		}
+
+		if !hasDif {
+			ans++
+		}
+	}
+
+	fmt.Println(ans)
+}
